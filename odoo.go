39d2c201955c30454e0fbff5490337ab138c14d5
Éric Vernichon <eric@vernichon.fr>
2016-03-03 04:16:46 +0100

    search and read example

--- odoo.go
+++ odoo.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/puper/go-jsonrpc/jsonrpc"
+	"jsonrpc"
 )
-func main() {
-	params := map[string]interface{}{"service":"object","method":"execute","args":[]interface{}{"oev8",1,"admin","res.partner","search",[]interface{}{}}}
-	conn  := jsonrpc.NewClient("http://127.0.0.1:8069/jsonrpc")
-	var response   *jsonrpc.Response
-	var erreur error
-	response, erreur = conn.Call("call",params)
-	if erreur == nil {
-		fmt.Printf("Response %s",response.Result)
-	}
-}
 
+func main() {
+	params := map[string]interface{}{"service": "object", "method": "execute", "args": []interface{}{"oev8", 1, "admin", "res.partner", "search", []interface{}{}}}
+	conn := jsonrpc.NewClient("http://odoo-server:8069/jsonrpc")
+	var response *jsonrpc.Response
+	var erreur error
+	response, erreur = conn.Call("call", params)
+	if erreur == nil {
+		fmt.Printf("Response %s", response.Result)
+	} else {
+		fmt.Printf("erreur %s", erreur)
+	}
+	params = map[string]interface{}{"service": "object", "method": "execute", "args": []interface{}{"oev8", 1, "admin", "res.partner", "read", response.Result}}
+	response, erreur = conn.Call("call", params)
+	if erreur == nil {
+		fmt.Printf("Response %s", response.Result)
+	} else {
+		fmt.Printf("erreur %s", erreur)
+	}
+
+}
